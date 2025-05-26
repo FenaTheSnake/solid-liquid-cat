5fc472535ecdb2d07e1faa41721f0c8ca3848dd7
Yaroslav Yazykov <fenathesnake@gmail.com>
2025-05-26 19:44:40 +0700

    final level, transition

--- Assets/Scripts/Game/GameState.cs
+++ Assets/Scripts/Game/GameState.cs
@@ -8,20 +8,48 @@ public class GameState
     public delegate void TransformationEventHandler(PlayerController whichController);
     public event TransformationEventHandler OnPlayerTransformation;
 
+    Transition _transition;
+    int _targetLevel;
+
     public void LoadLevel(int level)
     {
-        Level = level;
+        _targetLevel = level;
+        TransitionFadeIn();
 
-        SceneManager.LoadScene("Level" + level, LoadSceneMode.Single);
+        //Level = level;
+
+        //SceneManager.LoadScene("Level" + level, LoadSceneMode.Single);
     }
 
     public void RestartLevel()
     {
-        SceneManager.LoadScene("Level" + Level, LoadSceneMode.Single);
+        _targetLevel = Level;
+        TransitionFadeIn();
+        //SceneManager.LoadScene("Level" + Level, LoadSceneMode.Single);
     }
 
     public void PlayerTransformed(PlayerController whichController)
     {
         OnPlayerTransformation?.Invoke(whichController);
+    }
+
+    void TransitionFadeIn()
+    {
+        if(_transition == null) _transition = GameObject.Find("Transition").GetComponent<Transition>();
+        _transition.Show();
+    }
+    void TransitionFadeOut()
+    {
+        if(_transition == null) _transition = GameObject.Find("Transition").GetComponent<Transition>();
+        _transition.Hide();
+    }
+
+    public void OnTransitionFinished(bool transitionIsShownNow)
+    {
+        if (!transitionIsShownNow) return;
+
+        Level = _targetLevel;
+        SceneManager.LoadScene("Level" + _targetLevel, LoadSceneMode.Single);
+        TransitionFadeOut();
     }
 }
--- Assets/Scripts/Game/Transition/Transition.cs
+++ Assets/Scripts/Game/Transition/Transition.cs
@@ -1,16 +1,65 @@
 using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
 
 public class Transition : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    bool _fadeIn;
+    bool _activated;
+
+    float _alpha;
+
+    Image _image;
+
+    GameState _gameState;
+
+    [Inject]
+    void Construct(GameState gamestate)
     {
-        
+        _gameState = gamestate;
+
+        DontDestroyOnLoad(transform.parent.gameObject);
+        DontDestroyOnLoad(this);
+        _image = GetComponent<Image>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!_activated) return;
+
+        if (_fadeIn)
+        {
+            _image.color = new Color(_image.color.r, _image.color.g, _image.color.b, _alpha);
+            _alpha += 0.05f;
+            if (_alpha >= 1.25f)
+            {
+                _activated = false;
+                _gameState.OnTransitionFinished(true);
+            }
+        }
+        else
+        {
+            _image.color = new Color(_image.color.r, _image.color.g, _image.color.b, _alpha);
+            _alpha -= 0.05f;
+            if (_alpha <= -0.25f)
+            {
+                _activated = false;
+                _gameState.OnTransitionFinished(false);
+            }
+        }
+    }
+
+    public void Show()
+    {
+        _fadeIn = true;
+        _activated = true;
+        _alpha = 0;
+    }
+
+    public void Hide()
+    {
+        _fadeIn = false;
+        _activated = true;
+        _alpha = 1.0f;
     }
 }
